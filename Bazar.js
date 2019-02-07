ee6af0a821ab1866c08599968c745503e2532b20
robatb <44624325+robatb@users.noreply.github.com>
2019-02-06 21:03:54 -0300

    Create Bazar.js

---
+++ Bazar.js
@@ -0,0 +1,5 @@
+function loaditem(){
+    document.getElementById("pichulita").style.filter ="blur(5px)";
+    
+    document.getElementById("pichulon").innerHTML ="<div><button type='button'>Cerrar</button></div>";
+}
