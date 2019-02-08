55838d7e3dd0aed6c49944b552c118ae7f7fbc59
robatb <44624325+robatb@users.noreply.github.com>
2019-02-07 21:46:29 -0300

    Update Bazar.js

--- Bazar.js
+++ Bazar.js
@@ -1,5 +1,1 @@
-function loaditem(){
-    document.getElementById("pichulita").style.filter ="blur(5px)";
-    
-    document.getElementById("pichulon").innerHTML ="<div><button type='button'>Cerrar</button></div>";
-}
+
