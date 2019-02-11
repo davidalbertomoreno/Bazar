3a89465cee56f67f7ed0e55af2f7c65684674f18
Enurghia <44556187+Enurghia@users.noreply.github.com>
2019-02-11 20:00:37 -0300

    AAA

---
+++ Mauricio/src/java/Servlet/preparar.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlet;
+
+import Controlador.Consultas;
+import java.sql.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Mazamo Dafter Dark
+ */
+public class preparar extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+		HttpSession HS = request.getSession();
+		String usuario = (String)HS.getAttribute("usuario");
+		Consultas C = new Consultas();
+		ArrayList <String> imagen = new ArrayList<>();
+		ArrayList <String> nombre = new ArrayList<>();
+		ArrayList <String> alias = new ArrayList<>();
+        ResultSet RS =  C.cargarPokemons(usuario);
+		try{
+			while(RS.next()){
+				if(RS.getBoolean(9)){
+					imagen.add(RS.getString(1));
+					nombre.add(RS.getString(2));
+					alias.add(RS.getString(3));
+				}
+			}
+			request.setAttribute("imagen",imagen);
+			request.setAttribute("nombre",nombre);
+			request.setAttribute("alias",alias);
+			request.getRequestDispatcher("prep.jsp").forward(request, response);
+		}
+		catch(SQLException e){
+			System.err.println("Error: "+e);
+		}
+	}
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
