e0616b1a0a673350b0152a7fd67d40bc76a7b723
Caroidk <44623477+Caroidk@users.noreply.github.com>
2019-02-12 20:23:15 -0300

    Update Consulta.java

---
+++ Consulta.java
@@ -0,0 +1,264 @@
+package Controlador;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Carito
+ */
+public class Consulta extends Conexion {
+    
+    public int idPers (String username){
+        
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        
+        try{
+            String consulta = "select id from personajes where nombre = ?";
+            pst = getConexion().prepareStatement(consulta);
+            pst.setString(1, username);
+            rs = pst.executeQuery();
+            
+            while(rs.next()){
+                return rs.getInt(username);
+            }
+            
+        }catch (SQLException e){System.err.println("Error:\n"+e);}
+         finally{
+	    try{
+                if(getConexion()!=null) getConexion().close();
+                if(pst!=null) pst.close();
+		if(rs != null) rs.close();
+            }catch(SQLException e){System.err.println("Error:\n"+e);}
+        }
+        
+        return 0;
+    }
+    
+    public int idObj(String objeto){
+        
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        
+        try{
+           String consulta = "select id from bazares where nombre = ?";
+           pst = getConexion().prepareStatement(consulta);
+           pst.setString(1, objeto);
+           rs = pst.executeQuery();
+           
+           while(rs.next()){
+               return rs.getInt(1);
+           }
+        }catch(SQLException e){System.err.println("Error:\n"+e);}
+        finally{
+	    try{
+                if(getConexion()!=null) getConexion().close();
+                if(pst!=null) pst.close();
+	        if(rs != null) rs.close();
+            }catch(SQLException e){System.err.println("Error:\n"+e);}
+	}
+        return 0;
+
+    }
+    
+    
+    public boolean consultas (int objeto, int personaje){
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try{
+            
+            String consulta = "select * from mochila where fk_objeto=? and fk_personaje=?";
+            pst = getConexion().prepareStatement(consulta);
+            pst.setInt(1, objeto);
+            pst.setInt(2, personaje);
+            rs = pst.executeQuery();
+        
+            if(rs.next()){
+                return true;
+            }
+            
+        }catch(SQLException e){
+            System.err.println("Error: "+e);
+        }finally {
+            try {
+                 if(getConexion()!=null)getConexion().close();
+                 if(pst!=null)pst.close();
+                 if(rs!=null) rs.close();
+            } catch (Exception e) {
+                System.err.println("Error: "+e);
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean usarObj(int objeto, int personaje){
+        
+        PreparedStatement pst = null;
+          
+          try{
+              String consulta = "update mochila set cantidad= cantidad-1 where fk_objeto= ? and fk_personaje= ?";
+              pst = getConexion().prepareStatement(consulta);
+              pst.setInt(1, objeto);
+              pst.setInt(2, personaje);
+              
+              if(pst.executeUpdate()==1){
+                return true;
+              }
+          }catch(SQLException e){
+              System.err.println("Error: "+e);
+          }finally{
+               try {
+                if(getConexion()!=null) getConexion().close();
+                if(pst!=null) pst.close();
+            } catch (Exception e) {
+                System.err.println("Error: \n"+e);
+            }
+        }
+        
+        return false;
+    }
+    
+
+    public boolean comprarObj(int objeto, int personaje, int Cantidad){
+    
+          PreparedStatement pst = null;
+          
+          try{
+              String consulta = "update mochila set cantidad= cantidad+? where fk_objeto= ? and fk_personaje= ?";
+              pst = getConexion().prepareStatement(consulta);
+              pst.setInt(1, Cantidad);
+              pst.setInt(2, objeto);
+              pst.setInt(3, personaje);
+              
+              if(pst.executeUpdate()==1){
+                return true;
+              }
+          }catch(SQLException e){
+              System.err.println("Error: "+e);
+          }finally{
+               try {
+                if(getConexion()!=null) getConexion().close();
+                if(pst!=null) pst.close();
+            } catch (Exception e) {
+                System.err.println("Error: \n"+e);
+            }
+        }
+        
+        return false;
+    }
+         
+    public boolean regcomprar(int objeto, int personaje, int Cantidad){
+        
+        PreparedStatement pst = null;
+        
+        try {
+            String consulta = "insert into mochila (fk_objeto, fk_personaje, cantidad) values (?,?,?)";
+            pst = getConexion().prepareStatement(consulta);
+            pst.setInt(1, objeto);
+            pst.setInt(2, personaje);
+            pst.setInt(3, Cantidad);
+            
+            if(pst.executeUpdate()==1){
+                return true;
+            }
+        } catch (SQLException e) {
+            System.err.println("Error: "+e);
+        }finally{
+            try {
+                if(getConexion()!=null) getConexion().close();
+                if(pst!=null) pst.close();
+            } catch (Exception e) {
+                System.err.println("Error: \n"+e);
+            }
+        }
+        
+        return false;
+    }
+    
+    public int CantObj (int objeto, int personaje){
+        
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        
+        try{
+            String consulta = "select cantidad from mochila where fk_objeto= ? and fk_personaje = ?";
+            pst = getConexion().prepareStatement(consulta);
+            pst.setInt(1, objeto);
+            pst.setInt(2, personaje);
+            rs = pst.executeQuery();
+            
+            if(rs.next()){
+                return rs.getInt(1);
+            }
+            
+        }catch(SQLException e){System.err.println("Error:\n"+e);
+        }finally{
+            try{
+                if(getConexion()!=null) getConexion().close();
+                if(pst!=null) pst.close();
+                if(rs!=null) rs.close();
+            }catch(Exception e){System.err.println("Error:\n"+e);}
+        }
+        return 0;
+    }
+    
+    /*public int getCant(){
+        
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        
+        try{
+            String consulta = "select count(*) from bazares";
+            pst = getConexion().prepareStatement(consulta);
+            rs = pst.executeQuery(consulta);
+            
+            if(rs.next()){
+                return rs.getInt(1);
+            }
+        }catch(SQLException e){System.err.println("Error:\n"+e);
+        }finally{
+            try{
+                if(getConexion()!=null) getConexion().close();
+                if(pst!=null) pst.close();
+                if(rs!=null) rs.close();
+            }catch(Exception e){System.err.println("Error:\n"+e);}
+        
+        }
+        
+        return 0;
+    }
+    
+    public String getUrl(int objeto){
+        
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        
+        try{
+
+            String consulta = "select imagen from bazares where id = ?";
+            pst = getConexion().prepareStatement(consulta);
+            pst.setInt(1, objeto);
+            rs = pst.executeQuery();
+            if(rs.next()){
+                return rs.getString(objeto);
+            }              
+            
+            
+        }catch(SQLException e){System.err.println("Error:\n"+e);
+        }finally{
+            try{
+                if (getConexion()!=null) getConexion().close();
+                if (pst!=null) pst.close();
+                if (rs!=null) rs.close();
+            }catch(Exception e){System.err.println("Error:\n"+e);}
+        }
+        
+        return null;
+    }
+    
+    public static void main(String[] args){
+        Consulta con = new Consulta();
+        System.out.println(con.usarObj(6, 1));
+    }*/
+}
